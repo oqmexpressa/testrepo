1764c94bacdb241381d54dc1b1d316b561df0306
oqmexpressa <albertabone@gmail.com>
2022-08-27 16:16:50 -0400

    Create main.py
    Do some data stuff

---
+++ main.py
@@ -0,0 +1,11 @@
+clean_data = lambda *_, **__: None
+process_data = lambda *_, **__: None
+generate_data = lambda *_, **__: None
+remove_outliers = lambda *_, **__: None
+
+def f(raw_data):
+  data = clean_data(raw_data)
+  data = remove_outliers = lambda *_, **__: None
+  
+  results = process_data(data)
+  report = generate_report(results)
